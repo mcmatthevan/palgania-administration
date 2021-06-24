d3ddf2a23658c6878985e20af10dbc1c642ba43f
mcmatthevan <mcmattevan@gmail.com>
2021-06-25 00:59:15 +0200

    retour

--- root/home/minecraft/palgania_save/push_oby.py
+++ root/home/minecraft/palgania_save/push_oby.py
@@ -2,17 +2,6 @@
 import os
 from time import strftime as t
 os.chdir("/home/minecraft/palgania_save/github_saves/")
-print(os.listdir("../temp_saves/"))
-print(os.listdir())
-if len(os.listdir("../temp_saves/")) < len(os.listdir()):
-	for f in os.listdir():
-		if f not in os.listdir("../temp_saves/"):
-			os.system("git rm --cached {}".format(f))
-			os.system("rm {}".format(f))
-	os.system("git commit -am \"clear file commit\"")
-	os.system("git push")
-os.system("mv ../temp_saves/* ./")
-os.system("rmdir ../temp_saves/")
 date = "{}/{}/{} {}:{}".format(t("%d"),t("%m"),t("%Y"),t("%H"),t("%M"))
 for i,fichier in enumerate(sorted(os.listdir())):
 	os.system("git add {}".format(fichier))
